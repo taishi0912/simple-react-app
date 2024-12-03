7682d4f6477ae97327cc99e6f469d87649266eff
taishi0912 <56779621+taishi0912@users.noreply.github.com>
2024-12-04 00:48:22 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>Start editing to see some magic happen!</p>
+      </header>
+    </div>
+  );
+}
